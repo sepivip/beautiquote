fd7f07dd4473dbf09ed25dd703a1ca0c8c0f2924
Beka Zakaidze <bekaz@adwise.ge>
2023-04-11 20:29:46 +0400

    Some updates

--- app.js
+++ app.js
@@ -1,141 +1,174 @@
-const quoteInput = document.getElementById('quote-input');
-const outputCanvas = document.getElementById('output-canvas');
-const ctx = outputCanvas.getContext('2d');
-const downloadBtn = document.getElementById('download-btn');
+const elements = {
+    quoteInput: document.getElementById('quote-input'),
+    outputCanvas: document.getElementById('output-canvas'),
+    // downloadBtn: document.getElementById('download-btn'),
+    copyBtn: document.getElementById('copy-btn'),
+    backgroundOptionsContainer: document.getElementById('background-options'),
+};
 
-const backgroundOptions = document.getElementsByName('background');
-const boldOption = document.getElementById('bold');
-const italicOption = document.getElementById('italic');
+const ctx = elements.outputCanvas.getContext('2d');
+elements.outputCanvas.width = config.canvasWidth;
+elements.outputCanvas.height = config.canvasHeight;
 
-for (const option of backgroundOptions) {
-    option.addEventListener('change', () => {
-        generateImage();
+// elements.maxWidth.width = config.maxWidth;
+// elements.maxHeight.height = config.maxHeight;
+
+elements.quoteInput.addEventListener('input', generateImage);
+elements.quoteInput.addEventListener('paste', () => {
+  setTimeout(generateImage, 0);
+});
+elements.quoteInput.addEventListener('keydown', (e) => {
+    if (e.key === 'Enter') {
+      e.preventDefault();
+      const cursorPosition = elements.quoteInput.selectionStart;
+      elements.quoteInput.value = elements.quoteInput.value.slice(0, cursorPosition) + '\n' + elements.quoteInput.value.slice(cursorPosition);
+      elements.quoteInput.selectionStart = elements.quoteInput.selectionEnd = cursorPosition + 1;
+    }
+    setTimeout(generateImage, 0);
+  });
+  
+
+
+
+// elements.downloadBtn.addEventListener('click', downloadImage);
+elements.copyBtn.addEventListener('click', copyImageToClipboard);
+
+generateGradientButtons();
+generateImage();
+
+function drawText() {
+    const text = elements.quoteInput.value.trim() || config.text.default;
+    const lines = splitTextIntoLines(text);
+    const fontSize = calculateFontSize(lines);
+    const lineHeight = fontSize + 12;
+  
+    ctx.font = `${fontSize}px 'Poppins', sans-serif`;
+    ctx.fillStyle = '#ffffff';
+    ctx.textAlign = 'center';
+    ctx.textBaseline = 'middle';
+  
+    for (let i = 0; i < lines.length; i++) {
+      const line = lines[i];
+      const y = elements.outputCanvas.height / 2 - ((lines.length - 1) * lineHeight) / 2 + i * lineHeight;
+      ctx.fillText(line, elements.outputCanvas.width / 2, y);
+    }
+  }
+  
+  function calculateFontSize(lines) {
+    let fontSize = config.maxFontSize;
+  
+    while (fontSize >= config.minFontSize) {
+      ctx.font = `${fontSize}px 'Poppins', sans-serif`;
+      const isTextWidthExceeded = lines.some(line => ctx.measureText(line).width > elements.outputCanvas.width - 2 * config.padding);
+      const lineHeight = fontSize + 12;
+      const totalHeight = lines.length * lineHeight;
+  
+      if (!isTextWidthExceeded && totalHeight <= elements.outputCanvas.height - 2 * config.padding) {
+        return fontSize;
+      }
+  
+      fontSize -= 1;
+    }
+  
+    return config.minFontSize;
+  }
+  
+
+  function splitTextIntoLines(text) {
+    const paragraphs = text.split('\n');
+    const lines = [];
+  
+    paragraphs.forEach(paragraph => {
+      const words = paragraph.split(' ');
+      let currentLine = words[0];
+  
+      for (let i = 1; i < words.length; i++) {
+        const word = words[i];
+        const width = ctx.measureText(currentLine + ' ' + word).width;
+  
+        if (width < elements.outputCanvas.width - 2 * config.padding) {
+          currentLine += ' ' + word;
+        } else {
+          lines.push(currentLine);
+          currentLine = word;
+        }
+      }
+  
+      lines.push(currentLine);
     });
+  
+    return lines;
+  }
+  
+
+function generateImage() {
+    drawBackground();
+    drawText();
 }
 
-boldOption.addEventListener('change', () => {
-    generateImage();
-});
-
-italicOption.addEventListener('change', () => {
-    generateImage();
-});
-
-quoteInput.addEventListener('input', () => {
-    generateImage();
-});
-
-downloadBtn.addEventListener('click', () => {
-    outputCanvas.toBlob(blob => {
+async function downloadImage() {
+    elements.outputCanvas.toBlob(blob => {
         const link = document.createElement('a');
         link.href = URL.createObjectURL(blob);
         link.download = 'quote_image.png';
         link.click();
     });
-});
+}
+
+async function copyImageToClipboard() {
+    const dataUrl = elements.outputCanvas.toDataURL();
+    const imgBlob = await (await fetch(dataUrl)).blob();
+    const clipboardItem = new ClipboardItem({ 'image/png': imgBlob });
+    await navigator.clipboard.write([clipboardItem]);
+}
+
+// Restored gradient background functions
+function generateGradientButtons() {
+    config.gradients.forEach(gradient => {
+        const input = document.createElement('input');
+        input.type = 'radio';
+        input.name = 'background';
+        input.value = gradient.id;
+        input.id = 'background' + gradient.id;
+        input.checked = gradient.id === 1;
+        input.addEventListener('change', () => {
+            generateImage();
+        });
+        const label = document.createElement('label');
+        label.htmlFor = 'background' + gradient.id;
+        label.style.background = `linear-gradient(135deg, ${gradient.colors[0]} 0%, ${gradient.colors[1]} 100%)`;
+        label.style.width = '30px';
+        label.style.height = '30px';
+        label.style.cursor = 'pointer';
+        label.style.border = '1px solid transparent';
+        label.style.backgroundSize = 'cover';
+        label.style.marginRight = '5px';
+        label.style.borderRadius = '5px';
+
+        elements.backgroundOptionsContainer.appendChild(input);
+        elements.backgroundOptionsContainer.appendChild(label);
+    });
+}
 
 function getSelectedBackground() {
+    const backgroundOptions = document.getElementsByName('background');
     for (const option of backgroundOptions) {
         if (option.checked) {
-            return parseInt(option.value);
+            return config.gradients.find(gradient => gradient.id === parseInt(option.value));
         }
     }
 }
 
 function createGradient(background) {
-    const gradient = ctx.createLinearGradient(0, 0, 1200, 628);
-    if (background === 1) {
-        gradient.addColorStop(0, '#ff9a9e');
-        gradient.addColorStop(1, '#fad0c4');
-    } else {
-        gradient.addColorStop(0, '#84fab0');
-        gradient.addColorStop(1, '#8fd3f4');
-    }
+    const gradient = ctx.createLinearGradient(0, 0, elements.outputCanvas.width, elements.outputCanvas.height);
+    gradient.addColorStop(0, background.colors[0]);
+    gradient.addColorStop(1, background.colors[1]);
     return gradient;
 }
 
-function wrapText(context, text, maxWidth, maxHeight, lineHeight) {
-    const words = text.split(' ');
-    const lines = [];
-    let line = '';
-    const maxLines = Math.floor(maxHeight / lineHeight);
-
-    for (let n = 0; n < words.length; n++) {
-        const testLine = line + words[n] + ' ';
-        const metrics = context.measureText(testLine);
-        const testWidth = metrics.width;
-        if (testWidth > maxWidth && n > 0) {
-            if (lines.length < maxLines - 1) {
-                lines.push(line);
-            } else {
-                // Truncate the text if it exceeds the maximum number of lines
-                line = line.trim() + '...';
-                lines.push(line);
-                break;
-            }
-            line = words[n] + ' ';
-        } else {
-            line = testLine;
-        }
-    }
-    if (lines.length < maxLines) {
-        lines.push(line);
-    }
-    return lines;
-}
-
-
-function drawTextWithLineHeight(context, textLines, x, y, lineHeight) {
-    for (let i = 0; i < textLines.length; i++) {
-        context.fillText(textLines[i], x, y + i * lineHeight);
-    }
-}
-
-function generateImage() {
-    const quoteText = quoteInput.value;
-    if (!quoteText) {
-        downloadBtn.disabled = true;
-        return;
-    }
-
-    // Set canvas dimensions
-    outputCanvas.width = 1200;
-    outputCanvas.height = 628;
-
-    // Draw background gradient
-    const selectedBackground = getSelectedBackground();
-    const gradient = createGradient(selectedBackground);
+function drawBackground() {
+    const background = getSelectedBackground();
+    const gradient = createGradient(background);
     ctx.fillStyle = gradient;
-    ctx.fillRect(0, 0, 1200, 628);
-
-    // Set text style
-    const isBold = boldOption.checked;
-    const isItalic = italicOption.checked;
-    let fontStyle = '';
-
-    fontStyle += isBold ? 'bold ' : '';
-    fontStyle += isItalic ? 'italic ' : '';
-
-    ctx.font = fontStyle + '40px Arial';
-    ctx.fillStyle = '#ffffff';
-    ctx.textAlign = 'center';
-    ctx.textBaseline = 'middle';
-
-    // Draw text with word wrapping
-    const maxWidth = 1000; // Maximum width of the text area
-    const maxHeight = 500; // Maximum height of the text area
-    const lineHeight = 48; // Line height for word wrapping
-    const padding = 20; // Padding between text and canvas edges
-    const lines = wrapText(ctx, quoteText, maxWidth - padding * 2, maxHeight - padding * 2, lineHeight);
-
-    const textAreaHeight = lines.length * lineHeight;
-    const textY = (outputCanvas.height - textAreaHeight) / 2;
-
-    drawTextWithLineHeight(ctx, lines, outputCanvas.width / 2, textY, lineHeight);
-
-    // Enable downloading of the image
-    downloadBtn.disabled = false;
+    ctx.fillRect(0, 0, elements.outputCanvas.width, elements.outputCanvas.height);
 }
-
-// Initial image generation
-generateImage();
---
+++ config.js
@@ -0,0 +1,76 @@
+const config = {
+    // Canvas width
+    canvasWidth: 1200,
+    // Canvas height
+    canvasHeight: 628,
+    // Maximum width of the text area
+    maxWidth: 1100,
+    // Maximum height of the text area
+    maxHeight: 528,
+    // Maximum number of characters allowed before truncating the text
+    maxCharacters: 500,
+    // Minimum font size for the text (used when text length is equal to or greater than maxLength)
+    minFontSize: 10,
+    // Maximum font size for the text (used when text length is equal to or smaller than minLength)
+    maxFontSize: 100,
+    // Minimum text length (used to calculate font size)
+    minLength: 20,
+    // Maximum text length (used to calculate font size)
+    maxLength: 500,
+    // Padding between text and canvas edges
+    padding: 20,
+
+    gradients: [
+        {
+            id: 1,
+            name: 'Gradient 1',
+            colors: ['#5f2c82', '#49a09d'],
+        },
+        {
+            id: 2,
+            name: 'Gradient 2',
+            colors: ['#0F2027', '#2C5364'],
+        },
+        {
+            id: 3,
+            name: 'Gradient 3',
+            colors: ['#FC466B', '#3F5EFB'],
+        },
+        {
+            id: 4,
+            name: 'Gradient 4',
+            colors: ['#1a2a6c', '#b21f1f', '#fdbb2d'],
+        },
+        {
+            id: 5,
+            name: 'Gradient 5',
+            colors: ['#EB5757', '#000000'],
+        },
+        {
+            id: 6,
+            name: 'Gradient 6',
+            colors: ['#bdc3c7', '#2c3e50'],
+        },
+        {
+            id: 7,
+            name: 'Gradient 7',
+            colors: ['#ffd89b', '#19547b'],
+        },
+        {
+            id: 8,
+            name: 'Gradient 8',
+            colors: ['#833ab4', '#fd1d1d', '#fcb045'],
+        },
+        {
+            id: 9,
+            name: 'Gradient 9',
+            colors: ['#ADD100', '#7B920A'],
+        },
+        {
+            id: 10,
+            name: 'Gradient 10',
+            colors: ['#603813', '#b29f94'],
+        },
+        // Add more gradients here...
+    ],
+};
